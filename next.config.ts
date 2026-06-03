f43b04a4a19d749f9584e3083e05bb7707bd584e
buidinhhuy <buidinhhuy900@gmail.com>
2026-06-03 18:35:46 +0700

    test 5 for prisma

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      // require here so CI/Vercel installs the package before this file loads
+      // eslint-disable-next-line @typescript-eslint/no-var-requires
+      const { PrismaPlugin } = require('@prisma/nextjs-monorepo-workaround-plugin')
+      config.plugins = [...(config.plugins ?? []), new PrismaPlugin()]
+    }
+
+    return config
+  },
   images: {
     remotePatterns: [
       {
